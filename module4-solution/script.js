664ea1d21fe6582220d3382c70eb79e7f8b8b217
thangamari27 <144316551+thangamari27@users.noreply.github.com>
2024-01-25 22:07:21 +0530

    script.js

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -2,33 +2,24 @@
                   //Model_Solutiob_4
 //*************************************************//
 
-//1st step using wrap function
-(function(){
-    //2nd Step declare the variable
-    var names = ['Tom','Jerry','Jim','Joy','Max','Paul','Ben','Sam','Tmjk','John'];
+ var names=new Array();
+names[0]="Yaakov";
+names[1]="John";
+names[2]="Jen";
+names[3]="jason";
+names[4]="paul";
+names[5]="frank";
+names[6]="larry";
+names[7]="paula";
+names[8]="laura";
+names[9]="jim";
 
-    //3rd Using function of sayHello and sayBye
-    function sayHello(){
-        var wordHello = 'Hello';
-        console.log(wordHello+" "+names[i]);
-    }
-    function sayBye(){
-        var wordBye = "Bye";
-        console.log(wordBye+" " +names[i]);
-    }
 
-    for(i=0;i<names.length;i++){
-        //Check if the name of first letter === 'j' 
-        var firstLetter = names[i].charAt(0).toLowerCase();
-
-        if(firstLetter === 'j'){
-            
-            sayBye();       //calling sayBye function
-        }
-        else{
-            
-            sayHello();     //calling sayHello function
-        }
-    }
-
-})(); //calling wrap function
+for (var i = 0; i < names.length; i++) {
+	if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='j'){
+        console.log("Goodbye "+ names[i])
+	}
+	else{
+		console.log("Hello "+ names[i])
+	}
+}
